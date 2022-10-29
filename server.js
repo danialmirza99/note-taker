7944045b2110a06d8d605c4b3ecfb185caf695f0
danialmirza99 <danialmirza99@gmail.com>
2022-10-28 17:43:26 -0700

    added code ti server.js

--- server.js
+++ server.js
@@ -3,3 +3,20 @@ const path = require('path');
 
 const PORT = 3001;
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+app.use(express.static('public'));
+
+app.get('/', (req, res)=>
+    res.sendFile(path.join(__dirname, '/index.html'))
+);
+
+app.get('/feedback', (req,res) =>
+    res.sendFile(path.join(__dirname, '/notes.html'))
+);
+
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT}.`)
+);
